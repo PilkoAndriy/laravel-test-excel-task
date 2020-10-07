91a4f387f76bdf36348f689e0c602f0819de34fd
Pilko Andriy <pilkoandriy94@gmail.com>
2020-10-07 10:21:48 +0300

    Product page  route

--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\UploadExcelController;
+use App\Http\Controllers\ProductController;
 
 /*
 |--------------------------------------------------------------------------
@@ -18,3 +19,5 @@ use App\Http\Controllers\UploadExcelController;
 Route::get('/', [HomeController::class, 'index'])->name('home');
 
 Route::post('/upload/excel',[UploadExcelController::class,'store'])->name('upload.excel');
+
+Route::get('/products', [ProductController::class, 'index'])->name('product.index');
